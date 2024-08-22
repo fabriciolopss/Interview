f398b85dc28f90fa9b7ca4edf8f5624e34e3e4c5
fabriciolopss <fabriciolopss@ufmg.br>
2024-08-22 12:59:50 -0300

    fix: fix route

--- backend/contactBook/contactBookApi/urls.py
+++ backend/contactBook/contactBookApi/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ContactListView.as_view(), name='message-list'),
     path('create/', views.ContactCreateView.as_view(), name='message-create'),
-    path('comments/create/', views.CommentCreateView.as_view(), name='comment-create'),
     path('<uuid:contact_id>/comments/', views.ContactCommentsListView.as_view(), name='comment-list'),
     path('<uuid:contact_id>/comments/create/', views.CommentCreateView.as_view(), name='comment-create'),
 ]
